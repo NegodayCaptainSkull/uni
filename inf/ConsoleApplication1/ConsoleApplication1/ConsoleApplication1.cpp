a2c610167ebed0166a5d7a8233fc2d7064e4c3d6
NegodayCaptainSkull <84709250+NegodayCaptainSkull@users.noreply.github.com>
2025-09-15 13:43:03 +0300

    Add files via upload

---
+++ inf/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void theme3() {
+    double x;
+    double y;
+
+    cin >> x >> y;
+
+    int counter = 1;
+
+    while (x <= y) {
+        x *= 1.1;
+        counter++;
+    }
+
+    cout << counter;
+}
+
+void theme4q1() {
+    int32_t a;
+    int32_t b;
+    char operation;
+
+    cin >> a >> operation >> b;
+
+    int64_t result;
+    switch (operation) {
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+    case ':':
+        result = a / b;
+        break;
+    default:
+        result = 0;
+    }
+
+    cout << result;
+}
+
+void theme4q2() {
+    int A;
+    int B;
+
+    cin >> A >> B;
+
+    while (A > B) {
+        switch (A % 2) {
+        case 1:
+            A -= 1;
+            cout << "-1" << endl;
+            break;
+        case 0:
+            A /= 2;
+            cout << ":2" << endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    //theme3();
+    //theme4q1();
+    theme4q2();
+}
