50185cfc42c440da880091ff4d9efba58abd25a5
George <84709250+NegodayCaptainSkull@users.noreply.github.com>
2025-10-27 14:15:34 +0300

    5-7 tasks

---
+++ inf/KR2_Task_7_IKBO-70-25_VARIANT_13/KR2_Task_7_IKBO-70-25_VARIANT_13/KR2_Task_7_IKBO-70-25_VARIANT_13.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int findSumOfDigits(int start, long long number) {
+	int digit = number % 10;
+	long long leftNumber = number / 10;
+
+	if (leftNumber == 0) {
+		return start + digit;
+	}
+	else {
+		return findSumOfDigits(start + digit, leftNumber);
+	}
+}
+
+int main() {
+	long long number;
+	cout << "Input number to find sum of its digits: ";
+	cin >> number;
+
+	int sumOfDigits = findSumOfDigits(0, number);
+
+	cout << "Sum of digits: " << sumOfDigits;
+}
