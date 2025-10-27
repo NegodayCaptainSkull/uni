62b38bf06b30d2647bfa960a8cb9b9e686d16beb
George <84709250+NegodayCaptainSkull@users.noreply.github.com>
2025-10-27 14:14:21 +0300

    first 4 tasks

---
+++ inf/KR2_Task_1_IKBO-70-25_VARIANT_13/KR2_Task_1_IKBO-70-25_VARIANT_13/KR2_Task_1_IKBO-70-25_VARIANT_13.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+int random_int(int min, int max) {
+	static random_device rd;
+	static mt19937 gen(rd());
+	uniform_int_distribution<int> dist(min, max);
+	return dist(gen);
+}
+
+void vectorPrinter(vector<int> arr) {
+	cout << "Array: ";
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int main() {
+	int n;
+	cout << "Input length of an array: ";
+	cin >> n;
+
+	vector<int> array;
+
+	for (int i = 0; i < n; i++) {
+		array.push_back(random_int(-10000, 10000));
+	}
+
+	vectorPrinter(array);
+
+	vector<int> negativeNumbers;
+	vector<int> positiveNumbers;
+
+	for (int i = 0; i < n; i++) {
+		if (array[i] < 0) {
+			negativeNumbers.push_back(array[i]);
+		}
+		else {
+			positiveNumbers.push_back(array[i]);
+		}
+	}
+
+	vector<int> outputArray;
+	outputArray.insert(outputArray.end(), negativeNumbers.begin(), negativeNumbers.end());
+	outputArray.insert(outputArray.end(), positiveNumbers.begin(), positiveNumbers.end());
+
+	vectorPrinter(outputArray);
+}
---
+++ inf/KR2_Task_3_IKBO-70-25_VARIANT_13/KR2_Task_3_IKBO-70-25_VARIANT_13/KR2_Task_3_IKBO-70-25_VARIANT_13.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+int random_int(int min, int max) {
+	static random_device rd;
+	static mt19937 gen(rd());
+	uniform_int_distribution<int> dist(min, max);
+	return dist(gen);
+}
+
+void matrixPrinter(vector<vector<int>> matrix) {
+	cout << "Matrix: " << endl;
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[i].size(); j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "=================" << endl;
+}
+
+vector<vector<int>> matrixGenerator(int rows, int cols) {
+	vector<vector<int>> matrix(rows, vector<int>(cols));
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			matrix[i][j] = random_int(1, 21);
+		}
+	}
+
+	return matrix;
+}
+
+int main()
+{
+	int rows;
+	int cols;
+
+	cout << "Input rows and cols: ";
+	cin >> rows >> cols;
+
+	vector<vector<int>> matrix = matrixGenerator(rows, cols);
+
+	matrixPrinter(matrix);
+
+	cout << "Sums: ";
+	for (int i = 0; i < rows; i++) {
+		int rowSum = 0;
+		for (int j = 0; j < cols; j++) {
+			int number = matrix[i][j];
+			rowSum += number;
+		}
+
+		cout << rowSum << " ";
+	}
+}
